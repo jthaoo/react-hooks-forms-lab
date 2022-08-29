fed827dc2843b2860f409a52e713d74f349b3abd
Johnathan Thao <johnathanthao21@gmail.com>
2022-08-29 13:50:26 -0600

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,9 @@ function App() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
 
+  function handleItemFormSubmit(newItem) {
+    setItems([...items, newItem]);
+  } 
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
